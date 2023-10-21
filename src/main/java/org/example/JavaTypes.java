3885e66a2e4248593f8480cb8f7697e7461a94b5
home <liisimand@outlook.com>
2023-10-21 13:17:42 +0300

    Introducing java primitive types

---
+++ src/main/java/org/example/JavaTypes.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class JavaTypes {
+
+    public static void main(String[] args) {
+        System.out.println("Java types examples");
+        System.out.println("I want gin tonic");
+
+        //variable - place for storing some content
+        // has type and value
+        // int - integer number
+        // age - name of variable
+        int age;
+        age = 18;
+        age = 20;
+        System.out.println("Current age is:"+ age);
+        age = 25;
+        System.out.println("Current age is:"+ age);
+        int age2 = 18;
+        int age3 = -8;
+        System.out.println("Current age is:"+ age3);
+
+        int age4 = 4;
+
+        double age5 = 4.5;
+        System.out.println("Current age is:"+ age5);
+
+
+    }
+}
