f3accababcac39be5ffdcab9e9ce93f25fc9b1c1
home <liisimand@outlook.com>
2023-10-21 12:31:21 +0300

    My first java class created

---
+++ src/main/java/org/example/MyFirstApp.java
@@ -0,0 +1,12 @@
+package org.example;
+
+// use Code - Refactor to reformat source code
+public class MyFirstApp {
+
+    // psym
+    public static void main(String[] args) {
+        // use control + space to show context help
+        System.out.println('5');
+        System.out.println("5 5 5");
+    }
+}
