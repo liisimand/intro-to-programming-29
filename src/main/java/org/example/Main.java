a8bfdea0b42d2a4925cdecef6092fa0c0641066c
home <liisimand@outlook.com>
2023-10-21 11:32:15 +0300

    My first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // some useful shortcut
+        // line duplication
+        // mac: cmd + d
+        // windows: ctrl + d
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        //System.out.println("I'm Liisi");
+        //System.out.println("I'm Liisi");
+
+    }
+}
