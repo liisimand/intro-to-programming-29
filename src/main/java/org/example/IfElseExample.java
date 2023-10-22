e82fe0327d811896a9b159af4533960bb998e32c
home <liisimand@outlook.com>
2023-10-22 09:36:27 +0300

    if else examples - first

---
+++ src/main/java/org/example/IfElseExample.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class IfElseExample {
+
+    public static void main(String[] args) {
+        System.out.println("Let's make decision. Are you adult or kid?");
+
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("i'm adult");
+        }
+
+        if (age < 18) {
+            System.out.println("just a kid");
+        }
+
+    }
+}
